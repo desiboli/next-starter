f7379e0d2d7233a30e4dabcc4f85bcc97de1b815
desiboli <adam.benals@gmail.com>
2024-09-24 14:32:33 +0200

    typed env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { env } from "@/env/server";
+
 export default function Home() {
+  console.log(env.DATABASE_URL);
   return (
     <main className="h-full w-full">
       <h1>Hello World!</h1>
---
+++ src/env/server.ts
@@ -0,0 +1,29 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { ZodError, z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+    DATABASE_URL: z.string().url(),
+    // OPEN_AI_API_KEY: z.string().min(1),
+  },
+  // Called when the schema validation fails.
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  // Treat empty strings as undefined.
+  emptyStringAsUndefined: true,
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  // runtimeEnv: {
+  //   DATABASE_URL: process.env.DATABASE_URL,
+  //   OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  // },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+});
